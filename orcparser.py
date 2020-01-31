342af088d7a3e85fe249b1f31ddfed7d1924c5dd
qisu8313 <qisu@ucsb.edu>
2020-01-30 17:27:34 -0800

    fix wrong way of parsing offset; delete get_offset() as it is unnecessary

--- orcparser.py
+++ orcparser.py
@@ -1,4 +1,4 @@
-import struct
+
 
 class OrcParser:
     def __init__(self, content):
@@ -9,17 +9,8 @@ class OrcParser:
         for i in k:
             print(i, k[i])
 
-
     def get_permission(self, k):
         return bin(ord(k))[2:].zfill(3)
-
-
-    def get_offset(self, k):
-        if k:
-            return hex(struct.unpack("I", k)[0])
-        else:
-            return 0x0
-
 
     def parse_bytes(self, k):
         result = 0
@@ -28,7 +19,6 @@ class OrcParser:
             result |= (i << shift_amt)
             shift_amt += 7
         return result
-
 
     def get_text7(self):
         result = ""
@@ -39,26 +29,21 @@ class OrcParser:
             result += chr(self.content[self.current_index])
             self.current_index += 1
 
-
     def get_word28(self):
         word28 = self.content[self.current_index: self.current_index + 4]
         self.current_index += 4
         return word28
-
 
     def get_byte7(self):
         byte7 = self.content[self.current_index: self.current_index + 1]
         self.current_index += 1
         return byte7
 
-
     def get_filetype(self):
         return self.get_byte7()
 
-
     def get_bool(self):
         return self.get_byte7()
-
 
     def parse_symbol(self):
         name = self.get_text7()
@@ -71,9 +56,8 @@ class OrcParser:
             "name": name,
             "is_define": is_define,
             "section": section,
-            "offset": self.get_offset(offset),
+            "offset": hex(self.parse_bytes(offset)),
         }
-
 
     def parse_symbol_table(self):
         num_entries = self.get_word28()
@@ -88,7 +72,6 @@ class OrcParser:
             "symbols": symbols
         }
 
-
     def parse_relocation_table(self):
         num_entries = self.get_word28()
         num_entries = self.parse_bytes(num_entries)
@@ -102,7 +85,6 @@ class OrcParser:
             "relocations": relocations
         }
 
-
     def parse_relocation(self):
         offset = self.get_word28()
         section = self.get_word28()
@@ -110,7 +92,7 @@ class OrcParser:
         plus = self.get_word28()
         return {
             "section": section,
-            "offset": self.get_offset(offset),
+            "offset": hex(self.parse_bytes(offset)),
             "symbol": symbol,
             "plus": plus
         }
@@ -122,7 +104,7 @@ class OrcParser:
         size = self.get_word28()
         return {
             "permissions": self.get_permission(permissions),
-            "offset": self.get_offset(offset),
+            "offset": hex(self.parse_bytes(offset)),
             "name": name,
             "size": hex(self.parse_bytes(size))
         }
@@ -136,7 +118,6 @@ class OrcParser:
             sections[section["name"]] = section
         return sections
 
-
     def parse_segment(self):
         name = self.get_text7()
         offset = self.get_word28()
@@ -146,12 +127,11 @@ class OrcParser:
         # 0b10 for progbits and 0b01 for note
         return {
             "name": name,
-            "offset": self.get_offset(offset),
+            "offset": hex(self.parse_bytes(offset)),
             "base": base,
             "permissions": self.get_permission(permissions),
             "segmentType": segmentType
         }
-
 
     def parse_segment_table(self):
         num_entries = self.get_word28()
@@ -164,7 +144,6 @@ class OrcParser:
             "num_entries": num_entries,
             "segments": segments
         }
-
 
     def parse_orcfile_struct(self):
         header = self.get_text7()
